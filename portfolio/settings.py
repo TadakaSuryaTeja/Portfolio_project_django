b377b8a7b335c47bb2b1e38ef672cf13e43ce2db
Tadaka Surya Teja <suryateja233@gmail.com>
2022-04-25 15:24:30 +0530

    Added css file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# print("base path:", BASE_DIR)
+# print("static files dirs:", STATIC_ROOT)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
