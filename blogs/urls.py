808527f5434fba38ee90eea09bfa635aafc97a05
Tadaka Surya Teja <30947856+TadakaSuryaTeja@users.noreply.github.com>
2022-05-04 21:35:53 +0530

    Added navbar and signup (#4)
    Added navbar and signup (#4)

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name="home"),
     path(r'blogs/<int:blogs_id>', views.detail, name="detail"),
     path(r'projects/<int:projects_id>', views.project_details, name="project_details"),
-
+    path(r'signup', views.signup_form, name="sign_up"),
 ]
